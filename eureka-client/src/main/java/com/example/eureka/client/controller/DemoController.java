06c7c94157f3e57e3fc04e2a8f25d29b4bb59637
luoml <ml_luo@126.com>
2016-10-13 10:59:15 +0800

    modify hello

--- eureka-client/src/main/java/com/example/eureka/client/controller/DemoController.java
+++ eureka-client/src/main/java/com/example/eureka/client/controller/DemoController.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,10 +17,10 @@ public class DemoController {
     private DiscoveryClient client;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(@RequestParam("name") String name) {
+    public String hello() {
         ServiceInstance instance = client.getLocalServiceInstance();
-        logger.info("/hello, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + name);
+        logger.info("/hello, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
         
-        return "hello, " + name;
+        return "hello"; 
     }
 }
